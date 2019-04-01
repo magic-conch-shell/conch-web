8aacfaef55fbe0b48ce5a75922581cd07a3e6eac
villith <scott.donnelly790@gmail.com>
2019-03-31 21:00:33 -0400

    Added a profile picture section to the navbar

--- src/components/NavBar/NavBarAccount.tsx
+++ src/components/NavBar/NavBarAccount.tsx
@@ -1,18 +1,22 @@
+import * as React from 'react';
+
 import {
+  Avatar,
   Button,
   Menu,
   MenuItem,
   StyleRulesCallback,
   Theme,
   WithStyles,
-  withStyles,
+  withStyles
 } from '@material-ui/core';
-import classnames from 'classnames';
-import * as React from 'react';
+import { Redirect, RouteComponentProps, withRouter } from 'react-router-dom';
 
 import { IUser } from '../../interfaces/User';
+import { Link } from 'react-router-dom';
 import { ThemeTypes } from '../../themes/mainTheme';
-import { withRouter, RouteComponentProps, Redirect } from 'react-router-dom';
+import classnames from 'classnames';
+import { darken } from '@material-ui/core/styles/colorManipulator';
 
 export interface INavBarAccountProps extends WithStyles<typeof styles> {
   anchorEl: HTMLElement | undefined;
@@ -30,8 +34,15 @@ export interface INavBarAccountState {
 
 const styles: StyleRulesCallback<any> = (theme: Theme) => ({
   root: {},
+  avatar: {
+    margin: 'auto',
+    borderRadius: '5px'
+  },
   accountMenuButtonContainer: {
     display: 'flex',
+    '& .root:hover': {
+      backgroundColor: 'rgba(0,0,0,0)'
+    }
   },
   accountMenuButton: {
     color: theme.palette.primary.main,
@@ -51,12 +62,20 @@ const styles: StyleRulesCallback<any> = (theme: Theme) => ({
     fontSize: '1.5rem',
     fontWeight: 'lighter',
   },
+  userAccount: {
+    display: 'flex',
+    cursor: 'pointer',
+    padding: theme.spacing.unit / 2,
+    '&:hover': {
+      backgroundColor: darken(theme.palette.background.default, 0.1)
+    }
+  }
 });
 
 class NavBarAccount extends React.Component<
   RouteComponentProps<any> & INavBarAccountProps,
   INavBarAccountState
-> {
+  > {
   public render() {
     const {
       anchorEl,
@@ -76,35 +95,41 @@ class NavBarAccount extends React.Component<
         )}
       >
         {location.pathname === '/login' ||
-        location.pathname === '/register' ? null : !user ? (
-          <div className={classes.accountMenuButtonContainer}>
-            <Button
-              variant="text"
-              className={classes.accountMenuButton}
-              onClick={() => <Redirect to="/login" />}
-            >
-              Log In
+          location.pathname === '/register' ? null : !user ? (
+            <div className={classes.accountMenuButtonContainer}>
+              <Button
+                variant="text"
+                className={classes.accountMenuButton}
+                onClick={() => <Redirect to="/login" />}
+              >
+                Log In
             </Button>
-            {/* <div className={classes.verticalDivider}>|</div> */}
-            {/* <Button variant="text" className={classes.accountMenuButton}>
+              {/* <div className={classes.verticalDivider}>|</div> */}
+              {/* <Button variant="text" className={classes.accountMenuButton}>
               Sign Up
             </Button> */}
-          </div>
-        ) : (
-          <div className={classes.userAccount}>
-            <Button className={classes.accountMenuButton} onClick={handleClick}>
-              {user.nickname}
-            </Button>
-            <Menu
-              id="accountOptions-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
-            </Menu>
-          </div>
-        )}
+            </div>
+          ) : (
+              <>
+                <div className={classes.userAccount} onClick={handleClick}>
+                  <Button className={classes.accountMenuButton}>
+                    {user.nickname}
+                  </Button>
+                  <Avatar className={classes.avatar} src={user.profileUrl} />
+                </div>
+                <Menu
+                  id="accountOptions-menu"
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <Link to="/profile">
+                    <MenuItem>Profile</MenuItem>
+                  </Link>
+                  <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
+                </Menu>
+              </>
+            )}
       </div>
     );
   }

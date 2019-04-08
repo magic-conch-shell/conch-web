fc0b60c4d9d695596210a95e9791550eadb0d921
Scott Donnelly <scoot.donnelly@gmail.com>
2019-04-07 20:31:12 -0400

    Added QuestionStatus to a Question. Changed from enum -> type for Status

--- src/interfaces/Question.ts
+++ src/interfaces/Question.ts
@@ -1,10 +1,9 @@
-enum ResultStatusTypes {
-  NOT_SUBMITTED,
-  SUBMITTED,
-  ACCEPTED,
-  ANSWERED,
-  RESOLVED,
-}
+export type ResultStatusTypes =
+  'NOT_SUBMITTED' |
+  'SUBMITTED' |
+  'ACCEPTED' |
+  'ANSWERED' |
+  'RESOLVED';
 
 export interface IQuestion {
   content: string;
@@ -15,5 +14,14 @@ export interface IQuestion {
   updated_at: string;
   user_id: number;
   tags: number[];
+  question_status: IQuestionStatus;
+}
+
+export interface IQuestionStatus {
+  created_at: string;
+  id: number;
+  mentor_id: number | null;
+  question_id: number;
   status: ResultStatusTypes;
+  updated_at: string;
 }
